4150df60da2fa0f3a5dc9c25fab7439fef66eeb2
Maison Gulyas <maison.personal03@gmail.com>
2025-11-22 12:13:55 -0800

    Setup Scripts to run the empty world and start the controller

--- LiDAR_Simulation/setup.sh
+++ LiDAR_Simulation/setup.sh
@@ -244,10 +244,6 @@ if docker ps | grep -q heat_jazzy; then
     echo ""
     echo "Entering container..."
     docker exec -it heat_jazzy bash
-    # Make sure on the right directory
-    echo ""
-    echo "Entering the Sim Dir"
-    cd ~/heatsim_ws
 else
     echo "❌ Container failed to start. Check logs with:"
     echo "  docker logs heat_jazzy"
