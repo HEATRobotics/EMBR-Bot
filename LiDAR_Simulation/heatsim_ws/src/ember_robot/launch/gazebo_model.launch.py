30f79a24927347852a9c6e0060f82c8c88ea3711
Maison Gulyas <maison.personal03@gmail.com>
2026-01-31 23:50:09 -0800

    Initialized Thermal Camera Test World

--- LiDAR_Simulation/heatsim_ws/src/ember_robot/launch/gazebo_model.launch.py
+++ LiDAR_Simulation/heatsim_ws/src/ember_robot/launch/gazebo_model.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     )
 
     # relative path world
-    world_file_relative_path = os.path.join('worlds', 'test01.world')
+    world_file_relative_path = os.path.join('worlds', 'thermalTestArea.world')
 
     # absolute path to world
     world_path = os.path.join(
