c31811cea47fc7c11b189bfefcd82f6db75496a0
Maison Gulyas <maison.personal03@gmail.com>
2026-02-03 02:20:05 -0800

    Got URDF's somewhat working, broke most things

--- LiDAR_Simulation/heatsim_ws/sim.sh
+++ LiDAR_Simulation/heatsim_ws/sim.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd ~/heatsim_ws
+#Source ROS
+source /opt/ros/jazzy/setup.bash
 colcon build --symlink-install
+#Source Overlay
 source install/setup.bash
+#Launch
 ros2 launch ember_robot gazebo_model.launch.py
