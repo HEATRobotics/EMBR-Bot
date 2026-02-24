bbda7a6067366196cf3e1c14442b3602db0a8335
Maison Gulyas <maison.personal03@gmail.com>
2026-02-24 14:55:13 -0800

    Descriptions added and setup for Node Fusion

---
+++ LiDAR_Simulation/heatsim_ws/src/ember_robot/parameters/cutom_ros_topics/gps_imu_topic.py
@@ -0,0 +1,6 @@
+# Fusion script for creating the gps_imu node needed for custom ros topic /gps_imu (msg_interface/GPSAndIMU).
+#   This is the GPS position and orientation
+# Scripts thay may use this:
+# |- ros2_ws/src/embr/embr/hotspotLocator.py
+# |- ros2_ws/src/embr/embr/thermalStream.py
+# |- ros2_ws/src/embr/embr/sensors/thermal.py
